a5dc110d128990fc5a3b3f04d0ad2738cd0be331
Artur Klauser <Artur.Klauser@computer.org>
2019-12-06 18:16:08 +0100

    Improve cache to include web accesses from pymol
    The default test configuration now runs entirely without requiring network
    accesses. This also makes the test suite vastly faster. It now completes in
    < 10 seconds whereas without cache it takes > 1 minute.

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,16 +47,50 @@ def add_bool_option(parser, name, default=False, help='', **kwargs):
 
 
 def pytest_addoption(parser):
+    # Webcache options:
+    #
+    # This test suite uses a cache to speed up web accesses by the plugin and
+    # PyMOL. By default it is configured to run entirely isolated from the
+    # network, which also speeds up testing significantly. The options have the
+    # following meaning:
+    #   --[no-]webcache: (default: on)
+    #     Turn web cache on/off. If off, the original access paths are used (no
+    #     patching happens) and all other webcache* options are meaningless.
+    #   --[no-]webcache-load: (default: on)
+    #     Web cache data is loaded from the cache directory on session start.
+    #     Used to avoid network accesses for data that has been recorded and
+    #     saved in the cache directory.
+    #   --[no-]webcache-fetch: (default: off)
+    #     Cache misses are fetched over the network via the original access
+    #     paths. If off, any cache miss results in an exception.
+    #   --[no-]webcache-save: (default: off)
+    #     Web cache data is saved to the cache directory on session end.
+    #
+    # Common operations:
+    #   Running tests:
+    #     Use the default option settings.
+    #   Changing tests or writing new tests:
+    #     Chances are that you are affecting the accesses necessary to run the
+    #     tests. While debugging, to fetch any missing data, turn on
+    #       --webcache-fetch
+    #   Committing changed tests:
+    #     To repopulate the cache directory perform a passing run with
+    #       --no-webcache-load --webcache-fetch --webcache-save
+    #     Then git add/rm the respective changes in the cache directory.
+
     add_bool_option(parser,
-                    'webcache-save',
-                    default=False,
-                    help='Save web cache to directory after runs complete.')
+                    'webcache',
+                    default=True,
+                    help='Use a web cache to filter network traffic.')
     add_bool_option(parser,
                     'webcache-load',
                     default=True,
                     help='Load web cache from directory before runs start.')
-    # By default we isolate the test from network fetch dependencies.
     add_bool_option(parser,
                     'webcache-fetch',
                     default=False,
                     help='Fetch items missing in web cache from web.')
+    add_bool_option(parser,
+                    'webcache-save',
+                    default=False,
+                    help='Save web cache to directory after runs complete.')
